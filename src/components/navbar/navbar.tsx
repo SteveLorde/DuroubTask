578e9340a0c449ceb81c94304084f87ae92f94be
SteveLorde <mostafa.maher98@gmail.com>
2024-07-02 16:41:19 +0200

    more work

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,19 +1,29 @@
+import "./navbar.scss"
+import {Route} from "react-router-dom";
+
 export default function Navbar() {
 
     return <>
         <div>
             {/*Logo*/}
-            <img src="" />
+            <img className={"logo"} src="/logos/Logo%20-%20%20White.svg"  alt={"duroublogo"}/>
 
             {/*NavLinks*/}
             <div>
-
+                <ul>
+                    <li><Route path={""}>About Us</Route></li>
+                    <li><Route path={""}>Services</Route></li>
+                    <li><Route path={""}>News</Route></li>
+                    <li><Route path={""}>Careers</Route></li>
+                    <li><Route path={""}>Contact Us</Route></li>
+                </ul>
             </div>
 
             {/*Language Button*/}
             <div>
-                <button>
-
+                <button className={"lang-btn"}>
+                    <span>Eng</span>
+                    <span>ع</span>
                 </button>
             </div>
         </div>
